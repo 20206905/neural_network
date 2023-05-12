ac3e6f965f57909f5a511adebaeedf14fb5deef3
Hugh Culling <hughculling@icloud.com>
2023-05-12 18:31:49 +0100

    Commit

--- public/sketch.js
+++ public/sketch.js
@@ -1,4 +1,4 @@
-// Step 1: load data or create some data
+// Step 1: load data
 const data = [
   {
     xs: {
@@ -3493,67 +3493,8 @@ const data = [
 ];
 
 // ---------------------------------------------------------------
-
-// // Step 2: set your neural network options
-// const options = {
-//   task: "classification",
-//   debug: true,
-// };
-
-// // Step 3: initialize your neural network
-// const nn = ml5.neuralNetwork(options);
-
-// // Step 4: add data to the neural network
-// data.forEach((item) => {
-//   const inputs = {
-//     r: item.r,
-//     g: item.g,
-//     b: item.b,
-//   };
-//   const output = {
-//     color: item.color,
-//   };
-
-//   nn.addData(inputs, output);
-// });
-
-// // Step 5: normalize your data;
-// nn.normalizeData();
-
-// // Step 6: train your neural network
-// const trainingOptions = {
-//   epochs: 32,
-//   batchSize: 12,
-// };
-// nn.train(trainingOptions, finishedTraining);
-
-// // Step 7: use the trained model
-// function finishedTraining() {
-//   classify();
-// }
-
-// // Step 8: make a classification
-// function classify() {
-//   const input = {
-//     r: 255,
-//     g: 0,
-//     b: 0,
-//   };
-//   nn.classify(input, handleResults);
-// }
-
-// // Step 9: define a function to handle the results of your classification
-// function handleResults(error, result) {
-//   if (error) {
-//     console.error(error);
-//     return;
-//   }
-//   console.log(result); // {label: 'red', confidence: 0.8};
-// }
-
-// ---------------------------------------------------------------
-
 // Create training and testing datasets.
+
 let testingData = [];
 let dataLength;
 let randomNumber;
@@ -3585,15 +3526,19 @@ while (iterator < 70) {
   iterator++;
 }
 
+// Convert the array of objects into an array of arrays.
 const arrayOfArrays = testingData.map((obj) => Object.values(obj));
 
-// Step 2: set your neural network options
+//-----------------------------------------------------------------------
+// Set up neural network.
+
+// Step 2: set neural network options
 const options = {
   task: "regression",
   debug: true,
 };
 
-// Step 3: initialize your neural network
+// Step 3: initialize neural network
 const nn = ml5.neuralNetwork(options);
 
 // Step 4: add data to the neural network
@@ -3612,7 +3557,7 @@ data.forEach((item) => {
   nn.addData(inputs, output);
 });
 
-// Step 5: normalize your data;
+// Step 5: normalize data;
 nn.normalizeData();
 
 // Step 6: train your neural network
@@ -3628,24 +3573,8 @@ let MAE = 0;
 let MAPE = 0;
 let tempMAPE = 0;
 
-// Step 7: use the trained model
+// Step 7: Make predictions using the trained model.
 function finishedTraining() {
-  // predictMultiple();
-
-  // --------------------------------------------------
-
-  // if (i < 69) {
-  //   console.log(testingData[i]);
-  //   nn.predict(testingData[i], (err, result) => {
-  //     if (err) {
-  //       console.log(err);
-  //       return;
-  //     }
-  //     console.log(result[0]);
-  //     i++;
-  //     finishedTraining();
-  //   });
-  // }
   if (i < 69) {
     nn.predict(testingData[i], (err, result) => {
       if (err) {
@@ -3671,27 +3600,5 @@ function finishedTraining() {
   }
 }
 
-// Step 8: make a prediction.
-function predictMultiple() {
-  nn.predictMultiple(arrayOfArrays, handleResults);
-}
-
 let groundTruth = 0;
 let estimate = 0;
-
-// Step 9: define a function to handle the results of your classification
-function handleResults(error, result) {
-  if (error) {
-    console.error(error);
-    return;
-  }
-  console.log(result);
-
-  for (let i = 0; i < testingData.length; i++) {
-    groundTruth = testGroundTruth[i];
-    console.log("groundTruth = " + groundTruth);
-
-    estimate = result[i][0].weight;
-    console.log("estimate = " + estimate);
-  }
-}
